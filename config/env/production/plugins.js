bed36d631cbb7abc6bc12a0420d3f12622f5d2d9
samim <samimfazlu@gmail.com>
2023-05-13 23:05:40 +0600

    Prrpared For deployment

---
+++ config/env/production/plugins.js
@@ -0,0 +1,35 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST", "smtp.example.com"),
+        port: env("SMTP_PORT", 587),
+        auth: {
+          user: env("SMTP_USERNAME"),
+          pass: env("SMTP_PASSWORD"),
+        },
+        secure: false,
+      },
+      settings: {
+        defaultFrom: "batchbootcamp1@gmail.com",
+        defaultReplyTo: "batchbootcamp1@gmail.com",
+      },
+    },
+  },
+});
